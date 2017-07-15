52c01d0f62a3490b5f8a3d74ebd617fbd67ac911
Suvedh Srikanth <suvedhs@gmail.com>
2017-07-15 03:17:41 -0500

    completed obligatory part of project

--- js/etch.js
+++ js/etch.js
@@ -1,22 +1,71 @@
+var currentDim = 16;
 $(document).ready(
   function() {
-/*    var $div = document.createElement("div");
-    $div.style.width = "50px";
-    $div.style.height = "50px";
-    div.style.display = "inline-block";
-    $div.style.border = "1px solid blue";
-*/
     for(i=1;i<=256; i++) {
-      $div = "<div>" + i + "</div>";
+      $div = "<div>" + /*i + */"</div>";
+//    $div = '<div id="' + i + '">' + i + '</div>';
       $("#container").append($div);
 
     }
-    $("#container > div").css("width","30px");
-    $("#container > div").css("height","30px");
+    $("#container > div").css("width","50px");
+    $("#container > div").css("height","50px");
     $("#container > div").css("display","inline-block");
+
+    addColor();
   }
 );
 
+function addColor() {
+  $("#container > div").mouseenter(function() {
+    if($(this).css("background-color")== "rgba(0, 0, 0, 0)"){
+      $(this).css("background-color",getRandomColor());
+    }
+  });
+}
+
 function appendDiv(id) {
   $("div#container").append(id);
+}
+
+function clearBoard() {
+  $("#container > div").css("background-color","white");
+}
+
+function getDim() {
+  var txt;
+  var dimStr = prompt("How many squares per side would you like the new grid?","16");
+  if(dimStr != null && parseInt(dimStr) != currentDim && !isNaN(parseInt(dimStr)) ) {
+    currentDim = parseInt(dimStr);
+    removeBoard();
+    changeDim(currentDim);
+  }
+  else {
+    clearBoard();
+  }
+}
+
+function removeBoard() {
+  $("#container > div").remove();
+}
+
+function changeDim(numBoxes) {
+  var dim = 800.0/numBoxes;
+  var total = numBoxes*numBoxes;
+  $div = "<div>" + /*i + */"</div>";
+  for(i=0; i < total; i++){
+    $("#container").append($div);
+  }
+  $("#container > div").css("width",dim + "px");
+  $("#container > div").css("height", dim + "px");
+  $("#container > div").css("display","inline-block");
+
+  addColor();
+}
+
+function getRandomColor() {
+  var red = Math.floor(Math.random()*256);
+  var blue = Math.floor(Math.random()*256);
+  var green = Math.floor(Math.random()*256);
+  var color = "rgb(" + red + ", " + blue + ", " + green + ")";
+  return color;
 }
